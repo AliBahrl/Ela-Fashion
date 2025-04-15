1ecec030f90edc8c03dd79da16de7bb7f5f3cf55
AliBahrl <162575719+AliBahrl@users.noreply.github.com>
2025-04-15 22:41:06 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//////filter-products\\\\\\\\
+// init Isotope
+var $products = $('.all-products').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $products.isotope({ filter: filterValue });
+  });
+//////slider-swiper\\\\\\\\
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 15,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 15,
+      },
+    },
+  });
